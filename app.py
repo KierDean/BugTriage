202c0ea1654c70dbfb3f0d79498c88ccfb68b370
Kieran Thompson <kierandeanthompson@gmail.com>
2022-06-09 13:40:51 +0100

    Test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, requests
+from flask import Flask, request
+
 app = Flask(__name__)
 
 highurl = "https://prod-17.uksouth.logic.azure.com:443/workflows/232e0cfdb4f04a02be8b0ca021abe229/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d15OaKaplzQQgvAtA3thcBSzDxq92eM_7OeiVTiVonU"
@@ -7,9 +8,15 @@ apikey = "T6pFf9c3AHQKzMcnuHyeQqQE23AI578W01V94uw0mj965nAvBYklRYvu2l6frY3jm6IEHn
 @app.route('/', methods=['POST'])
 def home():
     data = request.data
-    for i in data:
+    r = request.post(highurl, data=data)
+    request.post(url = highurl, data = "Test from API")
+    print(r.text)
+
+    
+
+    '''for i in data:
         if i['Priority'] == 'High':
-            requests.post(url = highurl, data = data)
+            request.post(url = highurl, data = data)'''
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=9090)
