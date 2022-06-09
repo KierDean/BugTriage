e58b11ef662fb37967324d3b609a7c9b32ca452b
Kieran Thompson <kierandeanthompson@gmail.com>
2022-06-09 21:07:53 +0100

    test, blank flask app

--- app.py
+++ app.py
@@ -6,19 +6,20 @@ from azure.storage.queue import (
 )
 import os, uuid
 app = Flask(__name__)
-
+'''
 @app.route('/')
 def home():
     return "<h1>We're home<h>"
 
-@app.route('/addtoq', method=['POST'])
-def home():
-    connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
+@app.route('/addtoq')
+def home():    
     q_name = "highpriority"
+    connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     queue_client = QueueClient.from_connection_string(connect_str, q_name)
     message = "Pyton API testing 001"
     queue_client.send_message(message)
     return message
+    '''
 
 if __name__ == '__main__':
     app.run()
