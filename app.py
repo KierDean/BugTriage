69a4745b908f71de20f218e21a56f030fa6ff49f
Kieran Thompson <kierandeanthompson@gmail.com>
2022-06-09 15:41:12 +0100

    Test add route and POST request

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ from flask import Flask, request
 import requests
 
 app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def home():
+    bugData={
+        "Priority":"High","Bug ID":"11xh374","Build Number":"3.3.4","Reported on":"23/04/2022","Reason":"Defect","Environment":"macOS Sierra 10.12.6"
+    }
+    r = requests.post('https://prod-17.uksouth.logic.azure.com:443/workflows/232e0cfdb4f04a02be8b0ca021abe229/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d15OaKaplzQQgvAtA3thcBSzDxq92eM_7OeiVTiVonU',data=bugData)
+    print(r.text)
+
 '''
 bugData={
     "Priority":"High","Bug ID":"11xh374","Build Number":"3.3.4","Reported on":"23/04/2022","Reason":"Defect","Environment":"macOS Sierra 10.12.6"
@@ -20,8 +29,6 @@ def home():
     r = requests.post(highurl, data=data)
     requests.post(url = highurl, data = "Test from API")
     print(r.text)
-
-
 
     for i in data:
         if i['Priority'] == 'High':
