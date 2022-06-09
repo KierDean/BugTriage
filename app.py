1c9c2d4647ee31cc289dd3df6cbb6bfe652c3860
Kieran Thompson <kierandeanthompson@gmail.com>
2022-06-09 14:18:47 +0100

    test to queue

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-highurl = "https://prod-17.uksouth.logic.azure.com:443/workflows/232e0cfdb4f04a02be8b0ca021abe229/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d15OaKaplzQQgvAtA3thcBSzDxq92eM_7OeiVTiVonU"
-apikey = "T6pFf9c3AHQKzMcnuHyeQqQE23AI578W01V94uw0mj965nAvBYklRYvu2l6frY3jm6IEHnQSbs7V+AStJWuQIQ=="
+bugData={
+    "Priority":"High","Bug ID":"11xh374","Build Number":"3.3.4","Reported on":"23/04/2022","Reason":"Defect","Environment":"macOS Sierra 10.12.6"
+}
+r = requests.post('https://prod-17.uksouth.logic.azure.com:443/workflows/232e0cfdb4f04a02be8b0ca021abe229/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d15OaKaplzQQgvAtA3thcBSzDxq92eM_7OeiVTiVonU',data=bugData)
+print(r.text)
 
-@app.route('/', methods=['GET', 'POST'])
+'''
+highurl = "https://prod-17.uksouth.logic.azure.com:443/workflows/232e0cfdb4f04a02be8b0ca021abe229/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d15OaKaplzQQgvAtA3thcBSzDxq92eM_7OeiVTiVonU"
+apikey = 
+
+@app.route('/', methods=['POST'])
 def home():
-    data = request.data
-    r = request.post(highurl, data=data)
-    request.post(url = highurl, data = "Test from API")
+    
+    data = requests.data
+    r = requests.post(highurl, data=data)
+    requests.post(url = highurl, data = "Test from API")
     print(r.text)
 
-    
 
-    '''for i in data:
+
+    for i in data:
         if i['Priority'] == 'High':
             request.post(url = highurl, data = data)'''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=9090)
+    app.run(port=9090)
