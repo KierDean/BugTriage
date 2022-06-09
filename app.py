8f872870cdff8ae4121566f9bb79d4c3f0241bf0
Kieran Thompson <kierandeanthompson@gmail.com>
2022-06-09 20:34:15 +0100

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from azure.storage.queue import (
         QueueClient,
         BinaryBase64EncodePolicy,
@@ -11,15 +11,14 @@ app = Flask(__name__)
 def home():
     return "<h1>We're home<h>"
 
-@app.route('/addtoq', method=['GET'])
+@app.route('/addtoq', method=['POST'])
 def home():
     connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     q_name = "highpriority"
     queue_client = QueueClient.from_connection_string(connect_str, q_name)
     message = "Pyton API testing 001"
     queue_client.send_message(message)
-
-    return "<h1>Success</h1>"
+    return message
 
 if __name__ == '__main__':
     app.run()
