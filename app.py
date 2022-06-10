441e05ec86c4b030ad297139dc6bb8d8c690057a
Kieran Thompson <kierandeanthompson@gmail.com>
2022-06-10 10:47:42 +0100

    Bug fixes

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ from azure.storage.queue import (
 import os, uuid
 app = Flask(__name__)
 
-@app.route('/', method=['GET'])
+@app.route('/', methods=['GET'])
 def home():
     return "<h1>We're home<h>"
     
-'''
-@app.route('/addtoq')
-def home():    
+
+@app.route('/addtoq', methods=['POST'])
+def queue():    
     q_name = "highpriority"
     connect_str = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     queue_client = QueueClient.from_connection_string(connect_str, q_name)
     message = "Pyton API testing 001"
     queue_client.send_message(message)
     return message
-    '''
+
 
 if __name__ == '__main__':
     app.run()
